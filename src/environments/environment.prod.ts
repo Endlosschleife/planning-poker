3f10a751bd8d3d1d74ac681480d6e4e03a6cf74a
Torben <torben@LHIND-N010397.fritz.box>
2020-08-16 23:02:35 +0200

    chore: added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDEkM-miQEqGOWNy9UHkAAHviLiMepA4AU",
+    authDomain: "planning-poker-79526.firebaseapp.com",
+    databaseURL: "https://planning-poker-79526.firebaseio.com",
+    projectId: "planning-poker-79526",
+    storageBucket: "planning-poker-79526.appspot.com",
+    messagingSenderId: "62972260773",
+    appId: "1:62972260773:web:ecec99861d8e91a2edf84c"
+  }
 };
