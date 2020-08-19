15e552b8d9960c657ec59948949b9e0a0dc1bdbf
Torben <torben@LHIND-N010397.fritz.box>
2020-08-19 23:19:13 +0200

    feat: added some color to poker cards

--- src/app/planning-poker/models/poker-card.enum.ts
+++ src/app/planning-poker/models/poker-card.enum.ts
@@ -5,21 +5,21 @@ interface PokerCardValue {
 
 export class PokerCardEnum {
 
-  static readonly ZERO = new PokerCardEnum('ZERO', {text: '0'});
-  static readonly ONE_HALF = new PokerCardEnum('ONE_HALF', {text: '½'});
-  static readonly ONE = new PokerCardEnum('ONE', {text: '1'});
-  static readonly TWO = new PokerCardEnum('TWO', {text: '2'});
-  static readonly THREE = new PokerCardEnum('THREE', {text: '3'});
-  static readonly FIVE = new PokerCardEnum('FIVE', {text: '5'});
-  static readonly EIGHT = new PokerCardEnum('EIGHT', {text: '8'});
-  static readonly THIRTEEN = new PokerCardEnum('THIRTEEN', {text: '13'});
-  static readonly TWENTY = new PokerCardEnum('TWENTY', {text: '20'});
-  static readonly FOURTY = new PokerCardEnum('FOURTY', {text: '40'});
-  static readonly HUNDRED = new PokerCardEnum('HUNDRED', {text: '100'});
-  static readonly UNKNOWN = new PokerCardEnum('UNKNOWN', {icon: 'mdi-account-question'});
-  static readonly COFFEE = new PokerCardEnum('COFFEE', {icon: 'mdi-coffee'});
+  static readonly ZERO = new PokerCardEnum('ZERO', {text: '0'}, '#1C8A00');
+  static readonly ONE_HALF = new PokerCardEnum('ONE_HALF', {text: '½'}, '#029522');
+  static readonly ONE = new PokerCardEnum('ONE', {text: '1'}, '#05A069');
+  static readonly TWO = new PokerCardEnum('TWO', {text: '2'}, '#09A0AC');
+  static readonly THREE = new PokerCardEnum('THREE', {text: '3'}, '#0C62B7');
+  static readonly FIVE = new PokerCardEnum('FIVE', {text: '5'}, '#111EC3');
+  static readonly EIGHT = new PokerCardEnum('EIGHT', {text: '8'}, '#5615CE');
+  static readonly THIRTEEN = new PokerCardEnum('THIRTEEN', {text: '13'}, '#AF1AD9');
+  static readonly TWENTY = new PokerCardEnum('TWENTY', {text: '20'}, '#E520BC');
+  static readonly FOURTY = new PokerCardEnum('FOURTY', {text: '40'}, '#F02570');
+  static readonly HUNDRED = new PokerCardEnum('HUNDRED', {text: '100'}, '#FB372C');
+  static readonly UNKNOWN = new PokerCardEnum('UNKNOWN', {icon: 'mdi-account-question'}, '#444');
+  static readonly COFFEE = new PokerCardEnum('COFFEE', {icon: 'mdi-coffee'}, '#444');
 
-  private constructor(private readonly key, public readonly value: PokerCardValue) {
+  private constructor(private readonly key, public readonly value: PokerCardValue, public readonly color: string) {
   }
 
   toString(): string {
